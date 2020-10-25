1504da34c3dddc9f71ff19039ebd2c4828c979bb
wangminghui1 <wan09020708@163.com>
2020-10-25 23:41:18 +0800

    2222

---
+++ src/demoComponents/demo2/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.css'
+class Index extends React.Component{
+    constructor(){
+        super();
+    }
+
+    componentDidMount(){
+    }
+
+    render(){
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default Index;
